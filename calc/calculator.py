409eb8bf5b9e1f031cd2f4a7b6e734dab397d588
samerthpatel <srp2266@njit.edu>
2021-11-18 13:54:05 -0500

    calculator history

--- calc/calculator.py
+++ calc/calculator.py
@@ -2,37 +2,53 @@
 from calc.calculations.addition import Addition
 from calc.calculations.subtraction import Subtraction
 from calc.calculations.multiplication import Multiplication
+from calc.calculations.division import Division
+
 
 class Calculator:
     """ This is the Calculator class"""
     history = []
+
     @staticmethod
     def add_numbers(*args):
         """ adds list of numbers"""
         addition = Addition(args)
         Calculator.history.append(addition)
         return addition.get_result()
+
     @staticmethod
     def clear_history():
         """ Clear the calculation history"""
         Calculator.history.clear()
         return True
+
     @staticmethod
     def get_calculation(num):
         """ get a specific calculation from history"""
         return Calculator.history[num]
+
     @staticmethod
     def get_calculation_last():
         """ get last calculation from history"""
         return Calculator.history[-1]
+
     @staticmethod
     def subtract_numbers(*args):
         """ subtract a list of numbers from result"""
         subtraction = Subtraction(args)
         Calculator.history.append(subtraction)
         return subtraction.get_result()
+
+    @staticmethod
+    def divide_numbers(*args):
+        """ subtract a list of numbers from result"""
+        division = Division(args)
+        Calculator.history.append(division)
+        return division.get_result()
+
     @staticmethod
     def multiply_numbers(*args):
-        """ multiplication number from result"""
+        """ subtract a list of numbers from result"""
         multiplication = Multiplication(args)
+        Calculator.history.append(multiplication)
         return multiplication.get_result()
